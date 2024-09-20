5f51950a6f426f30e1da2b71956b548341551ad6
takeuchi-ao <takeuchi-ao@mainichi.co.jp>
2024-09-20 18:40:48 +0900

    Siteコンポーネントで1行を出力するよう変更

---
+++ components/Site.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <li v-if="site">
+      <h3>{{ site.name }}</h3>
+      <NuxtLink :to="site.url" target="_blank" class="text-decoration-none">
+        {{ site.url }}
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+interface Site {
+  name: string;
+  url: string;
+}
+
+const props = defineProps<{
+  site: Site | null;
+}>();
+</script>
+
+<style scoped>
+.text-decoration-none {
+  text-decoration: none;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,15 +9,32 @@
           Browse through the websites of universities and facilities offering
           talks and lectures:
         </v-typography>
-        <UniversityLinks />
+        <ul v-for="site in sites">
+          <Site :site="site" />
+        </ul>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script setup lang="ts">
-// Import the UniversityLinks component
-//import UniversityLinks from '../components/UniversityLinks.vue'
+import Site from '~/components/Site.vue';
+
+interface Site {
+  name: string;
+  url: string;
+}
+
+const sites: Site[] = [
+  { name: 'University of Tokyo', url: 'https://www.u-tokyo.ac.jp/' },
+  { name: 'Kyoto University', url: 'https://www.kyoto-u.ac.jp/' },
+  { name: 'Osaka University', url: 'https://www.osaka-u.ac.jp/' },
+  { name: 'Tokyo Institute of Technology', url: 'https://www.titech.ac.jp/' },
+  { name: 'Keio University', url: 'https://www.keio.ac.jp/' },
+  { name: 'Waseda University', url: 'https://www.waseda.jp/' },
+  { name: 'Sophia University', url: 'https://www.sophia.ac.jp/' },
+  { name: 'Nagoya University', url: 'https://www.nagoya-u.ac.jp/' },
+];
 </script>
 
 <style scoped>
